3056c47c5e943cb25b671fca9275517028dfffd7
Ridge Frederick <ridge.frederick@gmail.com>
2017-12-04 21:07:16 -0600

    Update namespaces for tests

--- day3/test/day3_test.exs
+++ day3/test/day3_test.exs
@@ -3,18 +3,18 @@ defmodule Day3Test do
   doctest Day3
 
   test "data from square 1 is carried 0 steps" do
-    assert Day3.find_square_dist(1) == 0
+    assert Day3.Part1.find_square_dist(1) == 0
   end
   
   test "data from square 12 is carried 3 steps" do
-    assert Day3.find_square_dist(12) == 3
+    assert Day3.Part1.find_square_dist(12) == 3
   end
 
   test "data from square 23 is carried 2 steps" do
-    assert Day3.find_square_dist(23) == 2
+    assert Day3.Part1.find_square_dist(23) == 2
   end
 
   test "data from square 1024 is carried 31 steps" do
-    assert Day3.find_square_dist(1024) == 31
+    assert Day3.Part1.find_square_dist(1024) == 31
   end
 end
